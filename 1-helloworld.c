e6b3432fc8a228546c07bac6563f3d866ceecc77
Cameron Pickett <pickett.cj@gmail.com>
2013-10-25 19:18:01 -0700

    First interview review program... Hello world!

---
+++ 1-helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Displays "Hello world!" then exits with success.
+ *
+ * @param argc
+ *        The count of shell parameters passed to this function
+ * @param argv
+ *        An array of all the shell args passed to this function
+ */
+int main(int argc, char **argv) {
+  printf("Hello world!\n");
+  return 0;
+}
